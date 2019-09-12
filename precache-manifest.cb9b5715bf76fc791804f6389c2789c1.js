94b6d17b6540dbe0fb3cb9907f3a39f62a5511d8
Mariah Mensah <mariahm207@gmail.com>
2019-09-12 10:11:42 -0500

    Updates

---
+++ precache-manifest.cb9b5715bf76fc791804f6389c2789c1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f760527feb40bbcffad3a303d796e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a358a471aa78d147e41",
+    "url": "/static/css/main.61fc4a6c.chunk.css"
+  },
+  {
+    "revision": "542ea47f5f778936792c",
+    "url": "/static/js/2.62f9e0fa.chunk.js"
+  },
+  {
+    "revision": "5a358a471aa78d147e41",
+    "url": "/static/js/main.a45eca66.chunk.js"
+  },
+  {
+    "revision": "0648e454a59965d32bd4",
+    "url": "/static/js/runtime~main.ae28c465.js"
+  },
+  {
+    "revision": "4f3a9ab4ef3ce097288ec2675fa7292c",
+    "url": "/static/media/1Liter.4f3a9ab4.jpg"
+  },
+  {
+    "revision": "bf4ee98930373273831f58fbc27bb2ae",
+    "url": "/static/media/250ML.bf4ee989.jpg"
+  },
+  {
+    "revision": "fc2f80da86e0b3b7bd70e26a5beaaa42",
+    "url": "/static/media/500ML.fc2f80da.jpg"
+  },
+  {
+    "revision": "64b56a05a7aab429e87b16e75127b9f9",
+    "url": "/static/media/Boxed-Confetti.64b56a05.png"
+  },
+  {
+    "revision": "9f7a52747f6b5cd79e82729143b0da98",
+    "url": "/static/media/Main.9f7a5274.png"
+  },
+  {
+    "revision": "4c6310fccfc80105884c113a28c726a3",
+    "url": "/static/media/facebook.4c6310fc.svg"
+  },
+  {
+    "revision": "3c8b19be5cca5ca9b225fa5966cea9c0",
+    "url": "/static/media/fact-circle.3c8b19be.png"
+  },
+  {
+    "revision": "341a67c538d67f9ce92005cf14255dc2",
+    "url": "/static/media/instagram.341a67c5.svg"
+  },
+  {
+    "revision": "6c9c4ede6656f5a9b60b93fd6592a647",
+    "url": "/static/media/large.6c9c4ede.png"
+  },
+  {
+    "revision": "4e76ebf87a24eaa62e08d69ef79c6814",
+    "url": "/static/media/medium.4e76ebf8.png"
+  },
+  {
+    "revision": "c1adb786b651a31d3a60d7c3e0a98611",
+    "url": "/static/media/our-facilities.c1adb786.png"
+  },
+  {
+    "revision": "66f3ed2a181b7f2e1b714997b6dbec1b",
+    "url": "/static/media/package-image.66f3ed2a.png"
+  },
+  {
+    "revision": "dab03660f0d4aebaff65471350904531",
+    "url": "/static/media/pledge-merch.dab03660.jpg"
+  },
+  {
+    "revision": "759f7d944a5ae3d1cbfbb22911841301",
+    "url": "/static/media/pledge.759f7d94.png"
+  },
+  {
+    "revision": "d0895f899725df8635a3c345f698afc0",
+    "url": "/static/media/small.d0895f89.png"
+  },
+  {
+    "revision": "dd9897b46278f98e88d312a6fa93d3fb",
+    "url": "/static/media/team.dd9897b4.png"
+  },
+  {
+    "revision": "631cd5664eb1d4a27681a21ca4ad6126",
+    "url": "/static/media/twitter.631cd566.svg"
+  }
+]);
